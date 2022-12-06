e2934bfa4cf8f43b3ad17a650580b6f0c73e76de
David Wright <hockeyguy411@gmail.com>
2022-12-06 12:11:27 -0500

    WIP, steps seem to be correct but they are failing somewhere.

--- day-5/src/day_5/core.clj
+++ day-5/src/day_5/core.clj
@@ -24,3 +24,33 @@
                  (zipmap (interleave (range 1 10)))
                  ))
 
+(defn move-crates-step [crates qty from to]
+  (let [moving-crates (take qty from)
+        from-crates (get crates from)
+        to-crates (get crates to)]
+    (-> crates
+        (assoc from (drop qty from-crates))
+        (assoc to (apply conj to-crates (take qty from-crates))))
+    )
+  )
+
+(defn move-crates [crates moves]
+  (loop [c crates
+         [[qty from to] & rem] moves]
+    (prn (str "qty: " qty ", from: " from ", to:" to))
+    (clojure.pprint/write (into (sorted-map) c))
+    (prn "\n")
+    (if (nil? qty)
+      c
+      (recur (move-crates-step c qty from to) rem))))
+
+; conj works exactly how we want for seq, but not vecs
+;(apply conj (seq [1 2 3 4]) [5 6])
+
+(comment ""
+         (into (sorted-map) (move-crates crates (take 0 moves)))
+         (into (sorted-map) (move-crates crates (take 1 moves)))
+         (into (sorted-map) (move-crates crates (take 2 moves)))
+         ,)
+
+(defn compute [crates moves] (clojure.string/join (map first (vals (into (sorted-map) (move-crates crates moves))))))
