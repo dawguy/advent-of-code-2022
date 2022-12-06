4934987628baaec1004da776ab8ccdbea806bb46
David Wright <hockeyguy411@gmail.com>
2022-12-06 12:49:40 -0500

    Day 5 working for simple case

--- day-5/src/day_5/core.clj
+++ day-5/src/day_5/core.clj
@@ -3,10 +3,14 @@
 (comment ""
          (def line (first (adv/parse txt {})))
          ,)
-
+(def n 9)
+(def max-initial-height 8)
 (def txt (slurp "./resources/data.txt"))
+(def n 3)
+(def max-initial-height 3)
+(def txt (slurp "./resources/simple-case-data.txt"))
 (def moves (->> (adv/parse txt {:split-by-character " "})
-                (drop 10)
+                (drop (+ 2 max-initial-height))             ; This might be wrong, as its height dependent not columns.
                 (map (fn [l] [(Integer/parseInt (get l 1))
                               (Integer/parseInt (get l 3))
                               (Integer/parseInt (get l 5))]))
@@ -14,15 +18,24 @@
 
 ; Seems like each crate is 3 spaces wide with 1 space as a gap
 ; Turning into a constant so I don't accidentally crash my computer with a misplaced cursor :')
-; (take 9 (range 1 (* 9 4) 4))
-(defn line-to-crates [line] (map #(if (= " " %) nil %) (map #(subs line % (+ % 1)) [1 5 9 13 17 21 25 29 33])))
-(def crates (->> (take 8 (adv/parse txt {}))
-                 (map line-to-crates)
-                 (apply interleave)
-                 (partition 8)
-                 (map #(drop-while nil? %))
-                 (zipmap (interleave (range 1 10)))
-                 ))
+; (take n (range 1 (* n 4) 4))
+(defn line-to-crates [n]
+  (fn [line]
+    (map #(if (= " " %) nil %)
+         (map #(if (< % (count line))
+                  (subs line % (+ % 1))
+                  nil
+                  ) (take n (range 1 (* n 4) 4))))
+    ))
+(defn crates [n max-initial-height] "Generates the crates, with n being the highest number shown on the crates"
+  (->> (adv/parse txt {})
+       (take max-initial-height)
+       (map (line-to-crates n))
+       (apply interleave)
+       (partition max-initial-height)
+       (map #(drop-while nil? %))
+       (zipmap (interleave (range 1 (inc n))))
+       ))
 
 (defn move-crates-step [crates qty from to]
   (let [moving-crates (take qty from)
@@ -37,8 +50,8 @@
 (defn move-crates [crates moves]
   (loop [c crates
          [[qty from to] & rem] moves]
-    (prn (str "qty: " qty ", from: " from ", to:" to))
     (clojure.pprint/write (into (sorted-map) c))
+    (prn (str "qty: " qty ", from: " from ", to:" to))
     (prn "\n")
     (if (nil? qty)
       c
@@ -48,9 +61,14 @@
 ;(apply conj (seq [1 2 3 4]) [5 6])
 
 (comment ""
-         (into (sorted-map) (move-crates crates (take 0 moves)))
-         (into (sorted-map) (move-crates crates (take 1 moves)))
-         (into (sorted-map) (move-crates crates (take 2 moves)))
+         (into (sorted-map) (move-crates (crates n max-initial-height) (take 0 moves)))
+         (into (sorted-map) (move-crates (crates n max-initial-height) (take 1 moves)))
+         (into (sorted-map) (move-crates (crates n max-initial-height) (take 2 moves)))
+         (into (sorted-map) (move-crates (crates n max-initial-height) (take 3 moves)))
+         (into (sorted-map) (move-crates (crates n max-initial-height) (take 4 moves)))
+         (into (sorted-map) (move-crates (crates n max-initial-height) moves))
          ,)
 
 (defn compute [crates moves] (clojure.string/join (map first (vals (into (sorted-map) (move-crates crates moves))))))
+
+(compute (crates n max-initial-height) moves)
