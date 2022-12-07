8a98992c16801592cc8881af55bfd1d15127f768
David Wright <hockeyguy411@gmail.com>
2022-12-07 12:51:39 -0500

    Advent Of Code Reader Enhancements
    Adds more parsing and string splitting capabilities as the existing
    logic was less well suited for day 7 behavior (and these enhancements
    would've been useful for day 5).

--- advent-reader/src/advent_reader/core.clj
+++ advent-reader/src/advent_reader/core.clj
@@ -3,36 +3,49 @@
 (defn parse-lines [lines apply-fn?]
   (if (not apply-fn?)
     lines
-    (map #(or (try
-                (Integer/parseInt %)
-                (catch NumberFormatException e nil))
-              (try
-                (Double/parseDouble %)
-                (catch NumberFormatException e nil))
-              %)
-         lines)))
-
+    (if (sequential? lines)
+      (map #(#'parse-lines % apply-fn?) lines)
+      (or (try
+            (Integer/parseInt lines)
+            (catch NumberFormatException e nil))
+          (try
+            (Double/parseDouble lines)
+            (catch NumberFormatException e nil))
+          lines))))
 (defn group-lines [lines apply-fn?]
   (if (not apply-fn?)
     lines
-    (filterv #(not (= "" (first %))) (partition-by #(not (= "" %)) lines))))
-
-(defn split-lines [lines split-by-character]
-  (if (nil? split-by-character)
+    (filterv #(not (= (ffirst %) "")) (partition-by #(= (first %) "") lines))))
+(defn split-lines [lines split-by-characters]
+  (if (empty? split-by-characters)
     lines
-    (let [regex (re-pattern split-by-character)]
-      (map #(clojure.string/split % regex) lines))))
-
+    (let [regex (re-pattern (first split-by-characters))]
+      (map #(identity (split-lines (clojure.string/split % regex)
+                                  (rest split-by-characters)))
+           lines))))
 (defn parse [txt opts]
-  (let [{:keys [parse-vals group-by-empty-line split-by-character]}
+  (let [{:keys [parse-vals group-by-empty-line split-by-characters]}
         (merge {
                 :parse-vals false          ; When input is a int/double
                 :group-by-empty-line false ; Creates additional groupings per empty line
-                :split-by-character nil
+                :split-by-characters []
                 } opts)]
     (-> txt
         (clojure.string/split #"\n")
+        (split-lines split-by-characters)
         (parse-lines parse-vals)
         (group-lines group-by-empty-line)
-        (split-lines split-by-character)
     )))
+
+(comment "Helper defs for development"
+         (def lines ["abc def ghj" "abc def-ghj hik"])
+         (def split-by-characters [" " "-"])
+         (def split-by-characters ["-" " "])
+         (def txt (slurp "./resources/advent-reader.txt"))
+         (def opts {:parse-vals          true
+                    :split-by-characters [" "]
+                    :group-by-empty-line false})
+         (def lines [["1" "2" "3"] ["3" "4" "5"] ["6" "-" "9"] ["9" "9" "9"]])
+         (def apply-fn? true)
+         (parse txt opts)
+         ,)
--- advent-reader/test/advent_reader/core_test.clj
+++ advent-reader/test/advent_reader/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [advent-reader.core :refer :all]))
 
-(deftest a-test
+(deftest parse-int-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+(deftest parse-separate-empty-lines-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
+
+(deftest parse-split-by-character-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
